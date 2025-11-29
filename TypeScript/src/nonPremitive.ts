5ac6c7c068dddf933ea66e4d4977cfa2c36c39dd
shohan12727 <aishohan001@gmail.com>
2025-11-29 09:59:33 +0600

    non-premitive data example

---
+++ TypeScript/src/nonPremitive.ts
@@ -0,0 +1,13 @@
+// array, object
+
+const bazarList: string[] = ["eggs", "fish", "milk"];
+
+console.log(bazarList);
+
+const mixArr: (string | number)[] = ["eggs", 1, "fish", 2, "meat", 3];
+
+console.log(mixArr);
+
+let coordinates: [number, number] = [20, 30];
+
+let myNameandRoll: [string, number] = ["shohan", 2];
--- TypeScript/src/premitive.ts
+++ TypeScript/src/premitive.ts
@@ -3,3 +3,7 @@ let x = undefined;
 x = 5;
 
 x = 'Five'
+
+
+const name: string = 'shohan';
+const age:number = 45;
