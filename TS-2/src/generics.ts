2d0ad82a9df994751316b5e3a399a5805af88592
shohan12727 <aishohan001@gmail.com>
2025-12-16 21:12:54 +0600

    generics

---
+++ TS-2/src/generics.ts
@@ -0,0 +1,13 @@
+type GenericArray<T> = Array<T>;
+
+
+const friends : GenericArray<string> = ["X", "Y", "Z"];
+
+const rollNumbers : GenericArray<number> = [22.34,45,56,9];
+
+const isEligibleList : GenericArray<boolean> = [true, false, true];
+
+console.log(friends, rollNumbers, isEligibleList)
+
+
+
