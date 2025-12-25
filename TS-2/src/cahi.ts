ed4918c680fc7c871325dd5f28962fa94693ef3c
shohan12727 <aishohan001@gmail.com>
2025-12-25 16:16:40 +0600

    function

---
+++ TS-2/src/cahi.ts
@@ -0,0 +1,30 @@
+function addTwo (num: number) {
+    return num +2
+}
+
+function getUpper(val:string) {
+    return val.toUpperCase();
+}
+// console.log(getUpper("Shohan"))
+
+
+function signUpUser(name:string, email:string, isPaid: boolean){}
+
+signUpUser("Shohan","shohan@gmail.com", false);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(addTwo(10))
