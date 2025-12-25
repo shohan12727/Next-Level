21d5c7a9d7bec73821a20a5b4a37f2422d6ef276
shohan12727 <aishohan001@gmail.com>
2025-12-25 16:29:36 +0600

    a better way to write function

--- TS-2/src/cahi.ts
+++ TS-2/src/cahi.ts
@@ -1,30 +1,25 @@
-function addTwo (num: number) {
-    return num +2
+function addTwo(num: number) {
+  return num + 2;
 }
-
-function getUpper(val:string) {
-    return val.toUpperCase();
+// console.log(addTwo(10))
+function getUpper(val: string) {
+  return val.toUpperCase();
 }
 // console.log(getUpper("Shohan"))
 
+function signUpUser(name: string, email: string, isPaid: boolean) {}
 
-function signUpUser(name:string, email:string, isPaid: boolean){}
+signUpUser("Shohan", "shohan@gmail.com", false);
 
-signUpUser("Shohan","shohan@gmail.com", false);
+function getValue(myVal: number): boolean | string {
+  if (myVal > 5) {
+    return true;
+  }
+  return "200 OK";
+}
 
+const heros: string[] = ["Thor", "SpiderMan", "IronMan"];
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// console.log(addTwo(10))
+console.log(heros.map((hero) => {
+  return `hero is ${hero}`;
+}))
