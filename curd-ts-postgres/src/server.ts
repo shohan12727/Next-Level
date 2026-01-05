9def6da4572a40f95f95f788f04d13c68c81f14d
shohan12727 <aishohan001@gmail.com>
2026-01-06 03:49:19 +0600

    updated

--- curd-ts-postgres/src/server.ts
+++ curd-ts-postgres/src/server.ts
@@ -11,19 +11,6 @@ const pool: any = new Pool({
   connectionString: `${process.env.CONNECTION_STR}`,
 });
 
-// const initDB = async () => {
-//   await pool.query(`
-//     CREATE TABLE IF NOT EXISTS users (
-//       id SERIAL PRIMARY KEY,
-//       name VARCHAR(100) NOT NULL,
-//       email VARCHAR(255) UNIQUE NOT NULL,
-//       password VARCHAR(255) NOT NULL,
-//       role VARCHAR(50) DEFAULT 'user',
-//       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-//       updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-//     );
-//   `);
-// };
 
 const initDB = async () => {
   await pool.query(`
