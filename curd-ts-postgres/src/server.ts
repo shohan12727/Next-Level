969e8e6a3dc0a4de60fafd798eaf54529abe8707
shohan12727 <aishohan001@gmail.com>
2026-01-06 14:11:11 +0600

    delete api created

--- curd-ts-postgres/src/server.ts
+++ curd-ts-postgres/src/server.ts
@@ -149,6 +149,32 @@ app.put("/users/:id", async (req: Request, res: Response) => {
   }
 });
 
+app.delete("/users/:id", async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(`DELETE FROM users WHERE id = $1`, [
+      req.params.id,
+    ]);
+
+    if (result.rowCount === 0) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "User deleted successfully",
+      data: result.rows[0], 
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
